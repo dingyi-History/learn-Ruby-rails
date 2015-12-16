a0d5f2eec0662d61c506ee6667ff738a70598fe5
丁一鸣 <315129552@qq.com>
2015-12-16 16:25:54 +0800

    def setup 来通用的地方

--- demos/sample_app/test/controllers/static_pages_controller_test.rb
+++ demos/sample_app/test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "Ruby on Rails App"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title","Home"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title","Help"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title","About"
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
