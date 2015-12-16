12410b7e7ec2c241e424e9bad78f0bec736f7826
丁一鸣 <315129552@qq.com>
2015-12-16 16:18:25 +0800

    simple_app

--- demos/sample_app/test/controllers/static_pages_controller_test.rb
+++ demos/sample_app/test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title","Home"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title","Help"
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title","About"
   end
 
 end
